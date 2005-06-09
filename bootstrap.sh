ace9094e97df91a0cea5f5efc6ee090d2dc87907
Camilla Berglund <elmindreda@elmindreda.org>
2005-06-09 12:49:08 +0000

    Work.

---
+++ bootstrap.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# This script does all the necessary work to get from a clean checkout
+# to being able to run configure as usual.
+
+if [ ! -f include/Ample ]; then
+  echo "This doesn't look right... I don't feel good about this"
+  exit 1
+fi
+
+aclocal && \
+autoheader && \
+automake -a && \
+automake && \
+autoconf && \
+echo "Bootstrap successful, now run configure"
+
