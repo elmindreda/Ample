a98b987abe2786aca2c111f28a2043a437a2ea2d
Camilla Berglund <elmindreda@elmindreda.org>
2005-06-23 11:47:55 +0000

    Bug fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 # This script does all the necessary work to get from a clean checkout
 # to being able to run configure as usual.
 
-if [ ! -f include/Ample ]; then
+if [ ! -f include/Ample.h ]; then
   echo "This doesn't look right... I don't feel good about this"
   exit 1
 fi
